3bd28c1e99ff343261757e4c7f2b54ccf4e4301f
demoks <demokstar@gmail.com>
2014-11-10 18:49:44 +0600

    update

---
+++ Payeer (NetCat)/fail.php
@@ -0,0 +1,8 @@
+<?
+
+$NETCAT_FOLDER = join(strstr(__FILE__, "/") ? "/" : "\\", array_slice(preg_split("/[\/\\\]+/", __FILE__), 0, -4)).( strstr(__FILE__, "/") ? "/" : "\\" );
+include_once ($NETCAT_FOLDER . "vars.inc.php");
+require ($INCLUDE_FOLDER . "index.php");
+
+header("Location: /lk/myorders/");
+}
--- ru.lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('MODULE_PAYEER', 'Система приема платежей Payeer');
-define('MODULE_PAYEER_DESCRIPTION', 'Систем приема тлатежей Payeer для модуля Интернет-магазин');
-
